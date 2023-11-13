d0ed55e51fea4b1c87185fb56c620afd5260fc30
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2023-11-13 18:28:40 +0200

    Homework 3 Third Commit

--- VehicleShowroomV2/main.cpp
+++ VehicleShowroomV2/main.cpp
@@ -6,74 +6,94 @@ using namespace std;
 
 int main(){
 
-    std::vector<MyVehicle::Vehicle*> vehicles;
+    int option = 0;
 
-    MyVehicle::Car c1("Volvo", "S60", 5, 4778, 1850, 1426, 391, 2039, 455, 180, 'P', 6.2);
-    MyVehicle::Car c3("Volvo", "S90", 5, 4969, 1879, 1436, 431, 2119, 455, 180, 'P', 6.5); 
-    MyVehicle::Truck t1("Volvo", "VNX", 2, 3689, 1646, 3850, 4781, 220, 120, 'D', 12.2);
-    vehicles.push_back(&c1);
-    vehicles.push_back(&c3);
-    vehicles.push_back(&t1);
+    cout << "Option '1': Constructor, Destructor, Copy Constructor, Overloaded Assignment Operator." << endl;
+    cout << "Option '2': Move Constructor, Inheritance." << endl;
+    cout << "Option '3': Vector, Template Class and Function, Unique and Share Pointer." << endl;
+    cout << endl << "Choose functionalities option:";
+    cin >> option;
+    switch(option) {
+        case 1: {
 
-    cout << "C1 AFTER CALLING CONSTRUCTOR" << endl;
-    c1.print();
-    cout << endl;
+            MyVehicle::Car c1("Volvo", "S60", 5, 4778, 1850, 1426, 391, 2039, 455, 180, 'P', 6.2);
+            MyVehicle::Car c3("Volvo", "S90", 5, 4969, 1879, 1436, 431, 2119, 455, 180, 'P', 6.5);
 
-    cout << "T1 AFTER CALLING CONSTRUCTOR" << endl;
-    t1.print();
-    cout << endl;
+            cout << "C1 AFTER CALLING CONSTRUCTOR" << endl;
+            c1.print();
+            cout << endl;
 
-    MyVehicle::Car c2 = c1;
-    vehicles.push_back(&c2);
-    cout << "C2 AFTER CALLING COPY CONSTRUCTOR" << endl;
-    c2.print();
-    cout << endl;
+            MyVehicle::Car c2 = c1;
+            
+            cout << "C2 AFTER CALLING COPY CONSTRUCTOR" << endl;
+            c2.print();
+            cout << endl;
+            
+            cout << "C3 AFTER CALLING CONSTRUCTOR" << endl;
+            c3.print();
+            cout << endl;
+            c2 = c3; // c2 should have the contents of c3
+            cout << "C2 AFTER USING OVERLOADED ASSIGNMENT OPERATOR ON C3" << endl;
+            c2.print();
+            cout << endl;
 
-    c1.~Car();
-    cout << "C1 AFTER CALLING DESTRUCTOR UPON C1" << endl;
-    c1.print();
-    cout << endl;
-    // some fields from the instance have been deallocated from memory and are not longer present
-    cout << "C2 AFTER CALLING DESTRUCTOR UPON C1" << endl;
-    c2.print();
-    cout << endl;
-    // this instance remains untouched from the effects of the destructor, that means the refferences between v1 and v2 are not the same
-    
-    cout << "C3 AFTER CALLING CONSTRUCTOR" << endl;
-    c3.print();
-    cout << endl;
-    c2 = c3; // c2 should have the contents of c3
-    cout << "C2 AFTER USING OVERLOADED ASSIGNMENT OPERATOR ON C3" << endl;
-    c2.print();
-    cout << endl;
+            c1.~Car();
+            cout << "C1 HAS BEEN ERASED" << endl;;
+            cout << endl;
+            // some fields from the instance have been deallocated from memory and are not longer present
+            cout << "C2 AFTER CALLING DESTRUCTOR UPON C1" << endl;
+            c2.print();
+            cout << endl;
+            // this instance remains untouched from the effects of the destructor, that means the refferences between v1 and v2 are not the same
+            
+            // calling destructor again to check if c2 is a deep or shallow copy of c3    
+            c3.~Car();
+            cout << "C3 HAS BEEN ERASED" << endl;
+            cout << endl;
+            cout << "C2 AFTER CALLING DESTRUCTOR UPON C3" << endl;
+            c2.print();
+            cout << endl;
+            // without overloading '=' operator, we get a shallow copy (same reference) with overloading '=' operator, we basically create a deep copy (just like with copy constructor) if not overload, C2 will have some of its content absent otherwise, the content will still be there
+        }
 
-    // calling destructor again to check if c2 is a deep or shallow copy of c3
-    cout << "C3 AFTER CALLING DESTRUCTOR UPON C3" << endl;
-    c3.~Car();
-    c3.print();
-    cout << endl;
-    cout << "C2 AFTER CALLING DESTRUCTOR UPON C3" << endl;
-    c2.print();
-    cout << endl;
-    // without overloading '=' operator, we get a shallow copy (same reference) with overloading '=' operator, we basically create a deep copy (just like with copy constructor) if not overload, C2 will have some of its content absent otherwise, the content will still be there
+        case 2: {
+            
+            MyVehicle::Car c1("Volvo", "S60", 5, 4778, 1850, 1426, 391, 2039, 455, 180, 'P', 6.2);
+            MyVehicle::Truck t1("Volvo", "VNX", 2, 3689, 1646, 3850, 4781, 220, 120, 'D', 12.2);
 
-    cout<<"C2 BEFORE CALLING MOVE CONSTRUCTOR" << endl;
-    c2.print();
-    cout << endl;
+            cout << "T1 AFTER CALLING CONSTRUCTOR" << endl;
+            t1.print();
+            cout << endl;
 
-    MyVehicle::Car c2_moved(std::move(c2));
-    vehicles.push_back(&c2_moved);
-    cout<<"C2_MOVED AFTER CALLING MOVE CONSTRUCTOR UPON C2" << endl;
-    c2_moved.print();
-    cout << endl;
-    
-    cout << "Until now, you have created " << vehicles.size() << " objects." << endl;
-    cout << endl;
+            MyVehicle::Car c2 = c1;
+            cout<<"C2 BEFORE CALLING MOVE CONSTRUCTOR" << endl;
+            c2.print();
+            cout << endl;
 
-    cout << "C2 AFTER CALLING MOVE CONSTRUCTOR" << endl;
-    cout << "It won't show anything because C2 is uninitialized" << endl;
-    c2.print();
-    cout << endl;
+            MyVehicle::Car c2_moved(std::move(c2));
+            cout<<"C2_MOVED AFTER CALLING MOVE CONSTRUCTOR UPON C2" << endl;
+            c2_moved.print();
+            cout << endl;
+
+            cout << "C2 AFTER CALLING MOVE CONSTRUCTOR" << endl;
+            cout << "It won't show anything because C2 is uninitialized" << endl;
+            c2.print();
+        }
+
+        case 3: {
+            
+            std::vector<MyVehicle::Vehicle*> vehicles;
+
+            MyVehicle::Car c1("Volvo", "S60", 5, 4778, 1850, 1426, 391, 2039, 455, 180, 'P', 6.2);
+            MyVehicle::Car c3("Volvo", "S90", 5, 4969, 1879, 1436, 431, 2119, 455, 180, 'P', 6.5);
+            
+            vehicles.push_back(&c1);
+            vehicles.push_back(&c3);
+
+            cout << "Until now, you have created " << vehicles.size() << " objects." << endl;
+            cout << endl;
+        }
+    }
 
     return 0;
 }
